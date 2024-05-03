9ebd673634d38c077759b712d30f02c9451aaac0
key-on423 <3531697760@qq.com>
2024-05-03 23:07:58 +0800

    jisuan

---
+++ 计算1到100所有数的和/here.c
@@ -0,0 +1,14 @@
+//#include <stdio.h>
+//
+//int main()
+//{
+//	int x,sum;
+//	sum = 0;
+//	for (x = 1;x <= 100;x++)
+//	{
+//		sum += x;
+//	}
+//	printf("一到一百的总和为%d\n", sum);
+//	printf("五月3日 轻松的一天\n");
+//	return 0;
+//}
